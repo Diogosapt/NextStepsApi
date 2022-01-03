287dff2ad6ccbb13a38d9d6f45ddc4924a08e866
Diogosapt <diogo.pinto@decode.pt>
2022-01-03 17:33:55 +0000

    fix tests

---
+++ NextSteps.Adpater.Mongo/tests/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextSteps.Adpater.Mongo.tests
+{
+    internal class test
+    {
+    }
+}
